dfb81af2fa459bdb0ab668e136b00efb1beabbe5
wespreece <preece.wes@gmail.com>
2022-01-26 23:51:52 +0100

    Added code remarks on CurrentDateTime and uodated documentation

--- src/CurrentDateTime.java
+++ src/CurrentDateTime.java
@@ -6,11 +6,16 @@ import java.time.format.DateTimeFormatter;
 
 public class CurrentDateTime {
 
+    //Variables
     private LocalDateTime currentDateTime;
     private LocalDate currentDate;
     private LocalTime currentTime;
     private DayOfWeek currentDayOfWeek;
 
+    /**
+     * Constructor
+     * @param dropOffTime value to determine time item is dropped off.
+     */
     public CurrentDateTime(String dropOffTime){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         this.currentDateTime = LocalDateTime.parse(dropOffTime, formatter);
@@ -19,6 +24,10 @@ public class CurrentDateTime {
         this.currentDayOfWeek = currentDateTime.getDayOfWeek();
     }
 
+    /**
+     * Method
+     * Move all dates to next day and set time to 00:00
+     */
     public void moveToNextDay(){
         this.currentDate = currentDate.plusDays(1);
         this.currentDateTime = currentDate.atStartOfDay();
@@ -26,23 +35,42 @@ public class CurrentDateTime {
         this.currentDayOfWeek =currentDate.getDayOfWeek();
     }
 
+    /**
+     * Getter
+     * @return the current date and time.
+     */
     public LocalDateTime getCurrentDateTime() {
         return currentDateTime;
     }
 
+    /**
+     * Setter
+     * Set the current date and time.
+     */
     public void setCurrentDateTime(LocalDateTime currentDateTime) {
         this.currentDateTime = currentDateTime;
     }
 
+    /**
+     * Getter
+     * @return the current date.
+     */
     public LocalDate getCurrentDate() {
         return currentDate;
     }
 
-
+    /**
+     * Getter
+     * @return the current time.
+     */
     public LocalTime getCurrentTime() {
         return currentTime;
     }
 
+    /**
+     * Getter
+     * @return current day of week.
+     */
     public DayOfWeek getCurrentDayOfWeek() {
         return currentDayOfWeek;
     }
